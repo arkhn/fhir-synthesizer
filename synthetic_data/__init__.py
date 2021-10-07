a5d23dfd9b296c3b834f5518fed018236d957c52
clementjumel <clement.jumel@gmail.com>
2021-10-07 16:36:26 +0200

    :sparkles: First notebook

--- synthetic_data/__init__.py
+++ synthetic_data/__init__.py
@@ -1,3 +1,10 @@
 from .hapi_to_json import hapi_to_json
 from .json_to_var import json_to_var
-from .metadata import ANON_REQUESTS, DATA_PATH, NOT_ANON_REQUESTS
+from .metadata import (
+    ALL_RESOURCE_TYPES,
+    ANON_REQUESTS,
+    ANON_RESOURCES_TYPES,
+    DATA_PATH,
+    NOT_ANON_REQUESTS,
+    NOT_ANON_RESOURCE_TYPES,
+)
