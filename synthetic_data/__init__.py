0a655f4d053d2f25acea2f4c22f2a1977c6ac6e2
louise <garnierlouise10@gmail.com>
2021-10-11 17:52:57 +0200

    :sparkles: Pipeline to create fake data

--- synthetic_data/__init__.py
+++ synthetic_data/__init__.py
@@ -1,3 +1,4 @@
+from .fake_data import create_fake_data
 from .getter import get_all_values
 from .hapi_to_json import hapi_to_json
 from .json_to_var import json_to_var
@@ -8,6 +9,7 @@ from .metadata import (
     DATA_PATH,
     NOT_ANON_REQUESTS,
     NOT_ANON_RESOURCE_TYPES,
-    SPECS_TO_DELETE,
-    SPECS_TO_SAMPLE,
+    OUTPUT_DATA_PATH,
+    PATHS_TO_DELETE,
+    PATHS_TO_SAMPLE,
 )
--- synthetic_data/getter.py
+++ synthetic_data/getter.py
@@ -3,9 +3,8 @@ from typing import Any
 from glom import glom
 
 
-def get_all_values(data: Any, resource_type: str, spec: Any, n_flatten: int) -> Any:
-    resource_data = data[resource_type]
-    glm = glom(resource_data, spec)
+def get_all_values(data: Any, spec: Any, n_flatten: int) -> Any:
+    glm = glom(data, spec)
 
     for i in range(n_flatten):
         glm = [x for y in glm if y for x in y]
