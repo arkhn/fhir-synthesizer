150cee1545aa62cc3cc56ea2a17f6a5cac57e354
louise <garnierlouise10@gmail.com>
2021-10-12 18:31:03 +0200

    :sparkles: General update: new requests, fake meta, ids and refs

--- synthetic_data/__init__.py
+++ synthetic_data/__init__.py
@@ -1,13 +1,4 @@
 from .fake_data import create_fake_data
 from .fetch_hapi import fetch_hapi
 from .getter import get_all_values
-from .metadata import (
-    ALL_RESOURCE_TYPES,
-    ANON_REQUESTS,
-    ANON_RESOURCES_TYPES,
-    DATA_PATH,
-    NOT_ANON_REQUESTS,
-    NOT_ANON_RESOURCE_TYPES,
-    PATHS_TO_DELETE,
-    PATHS_TO_SAMPLE,
-)
+from .metadata import DATA_PATH
--- synthetic_data/fake_data.py
+++ synthetic_data/fake_data.py
@@ -1,46 +1,102 @@
 import json
 import os
-from typing import Optional
 
 import numpy as np
-from glom import assign, delete
+from glom import PathAccessError, assign, delete, glom
 
 from .getter import get_all_values
-from .metadata import DATA_PATH, PATHS_TO_DELETE, PATHS_TO_SAMPLE
+from .metadata import DATA_PATH, PATHS_DEFAULT_VALUE, PATHS_ID_REF, PATHS_TO_DELETE, PATHS_TO_SAMPLE
 
 
-def create_fake_data(initial_data: dict, resources: Optional[list[str]] = None):
+def create_fake_data(initial_data: dict, id_suffix: str):
     """Create and save synthetic data created from `initial_data`, with some arguments removed and
     others sampled from `initial_data`.
 
     Args:
         initial_data: Real data, from which we want to sample fake information.
-        resources: Resources for which to create fake data; if None, create data for all resources
-            contained in `initial_data`.
+        id_suffix: Suffix to add to ids and refs.
     """
     os.makedirs(DATA_PATH, exist_ok=True)
 
-    if resources is None:
-        resources = list(initial_data.keys())
+    resources = list(initial_data.keys())
 
     for resource_name in resources:
-        if resource_name not in initial_data:
-            raise ValueError(f"Resource '{resource_name}' not found in the initial data.")
-        if resource_name not in PATHS_TO_DELETE:
-            raise ValueError(f"Resource '{resource_name}' not found in PATH_TO_DELETE.")
-        if resource_name not in PATHS_TO_SAMPLE:
-            raise ValueError(f"Resource '{resource_name}' not found in PATH_TO_SAMPLE.")
 
-        resource = initial_data[resource_name]
+        resource = {"entry": initial_data[resource_name]["entry"]}
 
-        for path in PATHS_TO_DELETE[resource_name]:  # type: ignore
-            for i in range(len(resource["entry"])):
-                delete(resource, path.format(i), ignore_missing=True)
+        values_to_sample = {}
+        if resource_name in PATHS_TO_SAMPLE:
+            for path, spec, n_flatten in PATHS_TO_SAMPLE[resource_name]:
+                values_to_sample[path] = get_all_values(
+                    data=resource, spec=spec, n_flatten=n_flatten
+                )
 
-        for path, spec, n_flatten in PATHS_TO_SAMPLE[resource_name]:
-            all_values = get_all_values(data=resource, spec=spec, n_flatten=n_flatten)
-            for i in range(len(resource["entry"])):
-                assign(resource, path.format(i), np.random.choice(all_values))
+        # First, gather manually participants and start/end for appointments
+        practitioners = []  # all the practitioners that participate
+        n_practitioners = []  # number of practitioners per activity
+        patients = []  # all the patients that participate
+        n_patients = []  # number of patients per activity
+        periods = []  # starts and ends of appointments
+        if resource_name == "activites_planifiees":
+            for entry in resource["entry"]:
+                n_pat, n_pract = 0, 0
+                participants = entry["resource"]["participant"]
+                for participant in participants:
+                    participant["actor"].pop("identifier", None)
+                    if "reference" in participant["actor"]:
+                        participant["actor"]["reference"] += id_suffix
+                    if participant["actor"]["type"] == "Patient":
+                        patients.append(participant)
+                        n_pat += 1
+                    elif participant["actor"]["type"] == "Practitioner":
+                        practitioners.append(participant)
+                        n_pract += 1
+                n_practitioners.append(n_pract)
+                n_patients.append(n_pat)
+
+                # Start / end
+                periods.append((entry["resource"]["start"], entry["resource"]["end"]))
+
+        for i, entry in enumerate(resource["entry"]):
+
+            # Update the ids and refs to add a suffix
+            if resource_name in PATHS_ID_REF:
+                for path in PATHS_ID_REF[resource_name]:
+                    try:
+                        assign(
+                            resource, path.format(i), f"{glom(resource, path.format(i))}{id_suffix}"
+                        )
+                    except PathAccessError:
+                        pass
+
+            # Delete attributes that must be deleted
+            if resource_name in PATHS_TO_DELETE:
+                for path in PATHS_TO_DELETE[resource_name]:  # type: ignore
+                    delete(resource, path.format(i), ignore_missing=True)
+
+            # Sample attributes that must be sampled
+            if resource_name in PATHS_TO_SAMPLE:
+                for path, all_values in values_to_sample.items():
+                    assign(resource, path.format(i), np.random.choice(all_values))
+
+            # Set default values
+            if resource_name in PATHS_DEFAULT_VALUE:
+                for path, default_value in PATHS_DEFAULT_VALUE[resource_name]:  # type: ignore
+                    assign(resource, path.format(i), default_value)
+
+            # In the case of an appointment, handle participants and periods manually
+            if resource_name == "activites_planifiees":
+                fake_participants = list(
+                    np.random.choice(patients, size=np.random.choice(n_patients), replace=False)
+                ) + list(
+                    np.random.choice(
+                        practitioners, size=np.random.choice(n_practitioners), replace=False
+                    )
+                )
+                assign(resource, f"entry.{i}.resource.participant", fake_participants)
+                fake_start, fake_end = periods[np.random.choice(len(periods))]
+                assign(resource, f"entry.{i}.resource.start", fake_start)
+                assign(resource, f"entry.{i}.resource.end", fake_end)
 
         with open(os.path.join(DATA_PATH, f"fake_{resource_name}.json"), "w") as f:
             json.dump(resource, f, ensure_ascii=False)
