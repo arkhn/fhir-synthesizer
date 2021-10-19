ffadd9c68ae897c1976ea457adcc77162adabe83
clementjumel <clement.jumel@gmail.com>
2021-10-19 11:58:32 +0200

    :sparkles: Round function for integers

--- okapy/tools/utils.py
+++ okapy/tools/utils.py
@@ -4,10 +4,15 @@ from glom import glom
 
 
 def glom_getter(data: Any, spec: Any, n_flatten: int) -> Any:
-    """Use glom to access a resource or attribute in the input `data`."""
+    """Use glom to access a resource or attribute in the input `data` and flatten it, if needed."""
     glm = glom(data, spec)
 
     for _ in range(n_flatten):
         glm = [x for y in glm if y for x in y]
 
     return glm
+
+
+def int_round(x: int, n: int) -> int:
+    """Return the multiple of `n` the closest to the integer `x`."""
+    return n * round(x / n)
