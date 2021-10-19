f63561e444a200ea83551b70e00d11d7a9df61b7
clementjumel <clement.jumel@gmail.com>
2021-10-19 18:44:31 +0200

    :sparkles: Main function

--- okapy/__init__.py
+++ okapy/__init__.py
@@ -1,4 +1,5 @@
 from .anonymize import anonymize
 from .fetch import fetch
+from .main import anonymization_pipeline
 from .metadata import RESOURCE_NAMES
 from .preprocess import preprocess_sampling_data
