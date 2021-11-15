1e08f97c04e8d8003c893d59b75a6dc040a8640a
clementjumel <clement.jumel@gmail.com>
2021-11-15 14:47:19 +0100

    :white_check_mark: Tests for okapy with mocked data

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,9 @@
+import json
 import os
 
 import pytest
 
-from okapy.tools.paths import TESTS_DATA_PATH
+from okapy.tools.paths import EXAMPLES_PATH, TESTS_DATA_PATH
 
 
 @pytest.fixture(scope="session")
@@ -10,3 +11,10 @@ def test_data_path() -> str:
     """Create the directory to output results in & output its path."""
     os.makedirs(TESTS_DATA_PATH, exist_ok=True)
     return TESTS_DATA_PATH
+
+
+@pytest.fixture(scope="session")
+def patients_actifs_example() -> dict:
+    """Read a "patient_actifs" example and output it."""
+    with open(os.path.join(EXAMPLES_PATH, "patients_actifs.json"), "r") as file:
+        return json.load(file)
--- tests/main_test.py
+++ tests/main_test.py
@@ -1,2 +1,13 @@
-def test_hello_world():
-    assert True
+from okapy import anonymization_pipeline
+
+
+def test_anonymization_pipeline(mocker, patients_actifs_example: dict, test_data_path):
+    # Mock the function `fetch` in okapy.main (i.e. don't enter the function, just output the
+    # `return_value`
+    mocker.patch("okapy.main.fetch", return_value=patients_actifs_example)
+
+    # Test the anonymization pipeline with the mocked values
+    anonymization_pipeline(
+        resource_names=["patients_actifs"],
+        output_dir=test_data_path,
+    )
