874f711cde417aeab12f51f89aaea55e2e360b5a
clementjumel <clement.jumel@gmail.com>
2021-11-15 14:47:19 +0100

    :white_check_mark: Tests for okapy with mocked data

--- okapy/tools/paths.py
+++ okapy/tools/paths.py
@@ -3,3 +3,6 @@ import os
 ROOT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 DATA_PATH = os.path.join(ROOT_PATH, "data")
 TESTS_DATA_PATH = os.path.join(DATA_PATH, "tests")
+
+TESTS_PATH = os.path.join(ROOT_PATH, "tests")
+EXAMPLES_PATH = os.path.join(TESTS_PATH, "examples")
