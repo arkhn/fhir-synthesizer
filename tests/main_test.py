a4b9c8590fe7b976c8cfc64e2822ec7b55ed31e3
clementjumel <clement.jumel@gmail.com>
2021-11-16 13:27:32 +0100

    :pencil2: Typo

--- tests/main_test.py
+++ tests/main_test.py
@@ -3,7 +3,7 @@ from okapy import anonymization_pipeline
 
 def test_anonymization_pipeline(mocker, patients_actifs_example: dict, test_data_path):
     # Mock the function `fetch` in okapy.main (i.e. don't enter the function, just output the
-    # `return_value`
+    # `return_value`)
     mocker.patch("okapy.main.fetch", return_value=patients_actifs_example)
 
     # Test the anonymization pipeline with the mocked values
